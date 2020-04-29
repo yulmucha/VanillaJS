cd88086df8b4230a2cecdd411a0860cab927b885
Yul <notkhaki@gmail.com>
2020-04-30 05:36:13 +0900

    Add the feature of drawing lines

--- Paint/app.js
+++ Paint/app.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector('.js-canvas');
 const canvasStyle = window.getComputedStyle(canvas);
-const CANVAS_WIDTH = parseInt(canvasStyle.width, 10);
-const CANVAS_HEIGHT = parseInt(canvasStyle.height, 10);
+const CANVAS_WIDTH = parseInt(canvas.getAttribute('width'));
+const CANVAS_HEIGHT = parseInt(canvas.getAttribute('height'));
 const currentColor = document.querySelector('.js-current-color');
 const palette = document.querySelector('.js-palette');
 const modeBtn = document.querySelector('.js-mode');
@@ -29,25 +29,46 @@ function handleMouseDown(event) {
     painting = true;
     if (filling) {
         ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    } else {
+        const { offsetX, offsetY } = event;
+        ctx.beginPath();
+        moveTo(offsetX, offsetY);
     }
+}
+
+function handleMouseMove(event) {
+    if (painting) {
+        const { offsetX, offsetY } = event;
+        ctx.lineTo(offsetX, offsetY);
+        ctx.stroke();
+    }
+}
+
+function handleMouseUp() {
+    painting = false;
+    ctx.closePath();
 }
 
 function init() {
     canvas.addEventListener('mousedown', handleMouseDown);
+    canvas.addEventListener('mousemove', handleMouseMove)
+    canvas.addEventListener('mouseup', handleMouseUp)
     modeBtn.addEventListener('click', handleModeClick);
 
     Array.from(palette.children).forEach((colorBtn) => {
         colorBtn.addEventListener('click', () => {
-            ctx.fillStyle = colorBtn.style.backgroundColor;
-            currentColor.style.backgroundColor = ctx.fillStyle;
+            const selectedColor = colorBtn.style.backgroundColor;
+            ctx.fillStyle = selectedColor;
+            ctx.strokeStyle = selectedColor;
+            currentColor.style.backgroundColor = selectedColor;
         });
     });
 
     const controlRange = document.querySelector('.js-range');
     ctx.lineWidth = controlRange.value;
 
-    // ctx.fillStyle = 'white';
-    // ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.fillStyle = 'black';
 }
 
